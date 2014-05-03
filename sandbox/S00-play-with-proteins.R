dedc2a8c19f8b6b4152e72937697b743c79bd3c3
Sebastian Gibb <mail@sebastiangibb.de>
2014-05-03 23:45:47 +0100

    add plot and metadata example

--- sandbox/S00-play-with-proteins.R
+++ sandbox/S00-play-with-proteins.R
@@ -2,7 +2,16 @@ library("Pbase")
 
 fas <- "swissprot_human_canonical_19_09_12.fasta"
 
-p <- Proteins(fas)[1:10]
+#p <- Proteins(fas)[1:10]
+p <- Proteins(fas)
 pfeatures(p)
-pp <- cleave(p[1:10])
+ametadata(p)
+
+#pp <- cleave(p[1:10])
+pp <- cleave(p, missedCleavages = 0)
 pfeatures(pp)
+
+## emulate subsetting
+p3 <- pp
+p3@aa <- p3@aa[1:3]
+plot(p3, from = 1, to = 20)
