ded2d86365d0020e5f1cf1676be1e78060d654c9
Sebastian Gibb <mail@sebastiangibb.de>
2014-05-07 10:52:24 +0100

    add calculateProteinCoverageSimple

--- R/ProteinCoverageSummary-functions.R
+++ R/ProteinCoverageSummary-functions.R
@@ -53,9 +53,53 @@
   ir
 }
 
+#' calculate coverage of pattern in subject.
+#' @param pattern IRangesList of interest (from mzID, ...)
+#' @param subject IRangesList to compare against (mostly from Proteins)
+#' @return double, NA for patterns that are outside of subjects.
+#' @noRd
+.calculateProteinCoverageSimple <- function(pattern, subject) {
+  stopifnot(is(pattern, "IRangesList"))
+  stopifnot(is(subject, "IRangesList"))
+
+  if (is.null(names(pattern))) {
+    stop("No names for ", sQuote("pattern"), " available!")
+  } else if (anyDuplicated(names(pattern))) {
+    stop("No duplicated names for ", sQuote("pattern"), " allowed!")
+  }
+  if (is.null(names(subject))) {
+    stop("No names for ", sQuote("subject"), " available!")
+  } else if (anyDuplicated(names(subject))) {
+    stop("No duplicated names for ", sQuote("subject"), " allowed!")
+  }
+
+  ## combine overlapping ranges
+  pattern <- reduce(pattern)
+
+  ## reorder patterns by subject names (introduces NULL for non matches)
+  orderedPattern <- as.list(pattern)[names(subject)]
+
+  ## replace NULL by empty IRanges()
+  orderedPattern[unlist(lapply(orderedPattern, is.null))] <- IRanges()
+  orderedPattern <- IRangesList(orderedPattern)
+
+  ## test that all patterns lie within the subject
+  patternEnd <- as.integer(lapply(end(orderedPattern), tail, n = 1L))
+  subjectEnd <- as.integer(lapply(end(subject), tail, n = 1L))
+  isOutside <- which(patternEnd > subjectEnd)
+
+  ## calculate coverage
+  coverage <- as.double(sum(width(orderedPattern))/width(subject))
+  coverage[isOutside] <- NA
+  names(coverage) <- names(subject)
+
+  coverage
+}
+
 #' @param pattern IRangesList of interest (from mzID, ...)
 #' @param subject IRangesList to compare against (mostly from Proteins)
 #' @return a LogicaList (TRUE for overlap)
+#' @noRd
 .calculateProteinCoverage <- function(pattern, subject) {
   stopifnot(is(pattern, "IRangesList"))
   stopifnot(is(subject, "IRangesList"))
