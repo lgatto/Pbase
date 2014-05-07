06217674095c7943ea013e488c64a5e5180b8716
Sebastian Gibb <mail@sebastiangibb.de>
2014-05-07 11:00:00 +0100

    replace old .calculateProteinCoverage by simpler version

--- R/ProteinCoverageSummary-functions.R
+++ R/ProteinCoverageSummary-functions.R
@@ -53,12 +53,16 @@
   ir
 }
 
-#' calculate coverage of pattern in subject.
+#' Calculates the coverage of patterns in subjects based on the width of the
+#' patterns. This method assumes that all patterns lie within a subject and the
+#' the names of patterns and subjects containing the correct accession.
+#' Caution: This is based purley on IRanges. No sequence based checks are
+#' involved! You have to make sure that you compare compareable sequences.
 #' @param pattern IRangesList of interest (from mzID, ...)
 #' @param subject IRangesList to compare against (mostly from Proteins)
 #' @return double, NA for patterns that are outside of subjects.
 #' @noRd
-.calculateProteinCoverageSimple <- function(pattern, subject) {
+.calculateProteinCoverage <- function(pattern, subject) {
   stopifnot(is(pattern, "IRangesList"))
   stopifnot(is(subject, "IRangesList"))
 
@@ -94,52 +98,6 @@
   names(coverage) <- names(subject)
 
   coverage
-}
-
-#' @param pattern IRangesList of interest (from mzID, ...)
-#' @param subject IRangesList to compare against (mostly from Proteins)
-#' @return a LogicaList (TRUE for overlap)
-#' @noRd
-.calculateProteinCoverage <- function(pattern, subject) {
-  stopifnot(is(pattern, "IRangesList"))
-  stopifnot(is(subject, "IRangesList"))
-
-  if (is.null(names(pattern))) {
-    stop("No names for ", sQuote("pattern"), " available!")
-  } else if (anyDuplicated(names(pattern))) {
-    stop("No duplicated names for ", sQuote("pattern"), " allowed!")
-  }
-  if (is.null(names(subject))) {
-    stop("No names for ", sQuote("subject"), " available!")
-  } else if (anyDuplicated(names(subject))) {
-    stop("No duplicated names for ", sQuote("subject"), " allowed!")
-  }
-
-  ## combine overlapping ranges
-  pattern <- reduce(pattern)
-
-  ## find overlap
-  subjectForest <- IntervalForest(subject)
-  hits <- as.matrix(findOverlaps(pattern, subjectForest, maxgap = 0L,
-                                 minoverlap = 1L, type = "within",
-                                 select = "all"))
-  ## turn Hits into a LogicalList
-  subwidth <- unlist(width(subject))
-
-  csw <- (cumsum(c(0L, subwidth)))
-
-  ## shift pattern per corresponding protein length (similar to
-  ## AAStringSet@ranges)
-  pattern <- .flatIRangesList(pattern[hits[, 1L]],
-                              shift = TRUE, shiftBy = csw[hits[, 2L]])
-
-  ## create large logical vector to generate coverage mask in a vectorized way
-  l <- Rle(logical(1L), lengths = sum(subwidth))
-  l[pattern] <- TRUE
-
-  l <- LogicalList(split(l, rep.int(seq_along(subject), subwidth)))
-  names(l) <- names(subject)
-  l
 }
 
 .calculateProteinCoverageSummary <- function(object) {
--- tests/testthat/test_ProteinCoverageSummary-functions.R
+++ tests/testthat/test_ProteinCoverageSummary-functions.R
@@ -2,29 +2,28 @@ context("ProteinCoverageSummary-functions")
 
 test_that(".calculateProteinCoverage", {
   irl1 <- IRangesList(IRanges(start = 1, end = 5),
-                      IRanges(start = 1, end = 15),
-                      IRanges(start = 1, end = 10))
+                      IRanges(start = 1, end = 16),
+                      IRanges(start = 1, end = 10),
+                      IRanges(start = 1, end = 20))
   irl2 <- IRangesList(IRanges(start = 1, end = 6),
-                      IRanges(start = 11, end = 15))
-  result <- LogicalList(P2 = logical(5),
-                        P3 = rep(c(FALSE, TRUE), c(10, 5)),
-                        P1 = rep(c(TRUE, FALSE), c(6, 4)))
+                      IRanges(start = 13, end = 16),
+                      IRanges(start = 15, end = 25))
+  result <- c("P2" = 0, "P3" = 0.25, "P1" = 0.6, "P4" = NA)
   expect_error(Pbase:::.calculateProteinCoverage(list(), irl1))
   expect_error(Pbase:::.calculateProteinCoverage(irl2, list()))
   expect_error(Pbase:::.calculateProteinCoverage(irl2, irl1),
                "No names for .*pattern.* available!")
-  names(irl2) <- c("P3", "P3")
+  names(irl2) <- rep("P3", 3)
   expect_error(Pbase:::.calculateProteinCoverage(irl2, irl1),
                "No duplicated names for .*pattern.* allowed!")
-  names(irl2) <- c("P1", "P3")
+  names(irl2) <- c("P1", "P3", "P4")
   expect_error(Pbase:::.calculateProteinCoverage(irl2, irl1),
                "No names for .*subject.* available!")
-  names(irl1) <- c("P3", "P3", "P1")
+  names(irl1) <- c("P3", "P3", "P1", "P4")
   expect_error(Pbase:::.calculateProteinCoverage(irl2, irl1),
                "No duplicated names for .*subject.* allowed!")
-  names(irl1) <- c("P2", "P3", "P1")
+  names(irl1) <- c("P2", "P3", "P1", "P4")
 
-  expect_identical(Pbase:::.calculateProteinCoverage(irl2, irl1),
-                   result)
+  expect_equal(Pbase:::.calculateProteinCoverage(irl2, irl1), result)
 })
 
