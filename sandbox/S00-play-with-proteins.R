2cd02e04fe2e832bda0ff0daa785e6c0b64101ef
Laurent <laurent.gatto@gmail.com>
2014-05-08 00:30:33 +0100

    small example fasta - cleave takes ages

--- sandbox/S00-play-with-proteins.R
+++ sandbox/S00-play-with-proteins.R
@@ -3,12 +3,13 @@ library("Pbase")
 fas <- "swissprot_human_canonical_19_09_12.fasta"
 
 #p <- Proteins(fas)[1:10]
-p <- Proteins(fas)
+(p <- Proteins(fas))
 pfeatures(p)
 ametadata(p)
+pmetadata(p)
 
 #pp <- cleave(p[1:10])
-pp <- cleave(p, missedCleavages = 0)
+(pp <- cleave(p, missedCleavages = 0))
 pfeatures(pp)
 
 ## emulate subsetting
