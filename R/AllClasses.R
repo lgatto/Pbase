ec8cf5549fea3509f4a804f50f253e999ca240f6
Sebastian Gibb <mail@sebastiangibb.de>
2014-05-08 12:26:18 +0100

    setup basic vailidity checks

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -17,27 +17,42 @@ setClass("Proteins",
 ################################################################################
 
 .validProteins <- function(object) {
-  ## TODO:
-  msg <- validMsg(NULL, isCurrent(object))
+    ## TODO: extend validity checks and write unit tests
+    msg <- validMsg(NULL, isCurrent(object))
 
-  #n <- length(object@seq)
+    if (anyDuplicated(seqnames(object))) {
+        msg <- validMsg(msg, paste0("Duplicated names in ", sQuote("seqnames"),
+                                    " are not allowed!"))
+    }
 
-  #if (nrow(object@mcols) != n) {
-  #  msg <- validMsg(msg, paste0("Number of rows in the metadata and ",
-  #                              "the number of sequences do not match!"))
-  #}
+    if (length(object@pranges) != length(object@aa)) {
+        msg <- validMsg(msg, paste0("Number of IRanges in @pranges and the ",
+                                    "number of sequences in @aa do not match!"))
+    }
 
-  #npfeatures <- length(object@pfeatures)
+    if (any(names(object@pranges) != names(object@aa))) {
+        msg <- validMsg(msg, paste0("Names of IRanges in @pranges and the ",
+                                    "names of sequences in @aa do not match!"))
+    }
 
-  #if (npfeatures && npfeatures != n) {
-  #  msg <- validMsg(msg, paste0("Number of IRanges in pfeatures and ",
-  #                              "the number of sequences do not match!"))
-  #}
-  if (is.null(msg)) {
-    return(TRUE)
-  }
+    if (length(object@aa) != nrow(object@aa@elementMetadata)) {
+        msg <- validMsg(msg, paste0("Number of rows in the ametadata and the ",
+                                    "number of sequences in @aa do not match!"))
+    }
 
-  msg
+    if (!is.null(object@pranges@unlistData@elementMetadata) &&
+        nrow(object@pranges@unlistData@elementMetadata) !=
+        sum(elementLengths(object@pranges))) {
+        msg <- validMsg(msg, paste0("Number of rows in the pmetadata and the ",
+                                    "number of IRanges in @pranges do not ",
+                                    "match!"))
+    }
+
+    if (is.null(msg)) {
+        return(TRUE)
+    }
+
+    msg
 }
 
 setValidity("Proteins", .validProteins)
