dd6055fc72dd494cb5082c5194a1942994463627
Sebastian Gibb <mail@sebastiangibb.de>
2014-05-09 18:37:32 +0100

    add environments.R (counterpart to MSnbase/R/environments.R)

--- R/aa-functions.R
+++ R/aa-functions.R
@@ -3,10 +3,10 @@
 #' @return a named doubled vector
 #' @noRd
 .calculateMolecularWeight <- function(x) {
-    water <- sum(get.atomic.mass()[c("H", "H", "O")])
+    water <- sum(.get.atomic.mass()[c("H", "H", "O")])
 
-    aamass <- setNames(get.amino.acids()$ResidueMass,
-                       get.amino.acids()$AA)
+    aamass <- setNames(.get.amino.acids()$ResidueMass,
+                       .get.amino.acids()$AA)
 
     ppmass <- unlist(lapply(strsplit(as.character(x), ""), function(p) {
         sum(aamass[p])
