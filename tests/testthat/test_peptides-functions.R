346b810786259c09ba599e1830ec89b319d704f6
Laurent Gatto <l.gatto@dnavision.be>
2015-01-05 22:55:11 +0000

    change output format of .peptidePosition
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Pbase@98015 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/testthat/test_peptides-functions.R
+++ tests/testthat/test_peptides-functions.R
@@ -26,6 +26,14 @@ test_that(".peptidePosition", {
     subject <- c(P1 = "ACEDE", P3 = "JKLMN", P2 = "FGHIL")
     result <- IRangesList(c(P1 = IRanges(1, 3),
                             P2 = IRanges(4, 5)))
+    result <- IRanges(start = c(1,   # P1
+                                4),  # P2
+                      end = c(3,     # P1
+                              5))    # P2
+    mcols(result) <- DataFrame(PeptideIndex = Rle(c(1, 2)),
+                               ProteinIndex = Rle(c(1, 3)))
+    result <- split(result, f = c("P1", "P2"))
+
     expect_error(Pbase:::.peptidePosition(unname(pattern), subject),
                  "No names for .*pattern.* available!")
     expect_error(Pbase:::.peptidePosition(pattern, unname(subject)),
@@ -39,10 +47,24 @@ test_that(".peptidePosition", {
     ## multiple peptides per protein
     ## see https://github.com/ComputationalProteomicsUnit/Pbase/issues/5
     pattern <- c(P1 = "DE", P2 = "IL", P3 = "KR", P1 = "ABC", P3 = "LMN")
-    subject <- c(P1 = "ACEDE", P3 = "JKLMN", P2 = "FGHIL")
-    result <- IRangesList(c(P1 = IRanges(c(4, 1), c(5, 3)),
-                            P2 = IRanges(4, 5),
-                            P3 = IRanges(3, 5)))
+    pattern2 <- AAStringSetList(P1 = AAStringSet(c("DE", "ABC")),
+                                P2 = AAStringSet("IL"),
+                                P3 = AAStringSet(c("KR", "LMN")))
+    subject <- c(P1 = "ABCDE", P3 = "JKLMN", P2 = "FGHIL")
+    result <- IRanges(start = c(4, 1,   # P1
+                                4,      # P2
+                                3),     # P3
+                      end = c(5, 3,     # P1
+                              5,        # P2
+                              5))       # P3
+    result2 <- result
+    mcols(result) <- DataFrame(PeptideIndex = Rle(c(1, 4, 2, 5)),
+                               ProteinIndex = Rle(c(1, 1, 3, 2)))
+    mcols(result2) <- DataFrame(PeptideIndex = Rle(c(1, 2, 3, 5)),
+                                ProteinIndex = Rle(c(1, 1, 3, 2)))
+    result <- split(result, f = c("P1", "P1", "P2", "P3"))
+    result2 <- split(result2, f = c("P1", "P1", "P2", "P3"))
     expect_equal(Pbase:::.peptidePosition(pattern, subject), result)
+    expect_equal(Pbase:::.peptidePosition(pattern2, subject), result2)
 })
 
