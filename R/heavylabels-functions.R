495fa0fcadecd8cde970c42b4cf69e3d5049212e
l.gatto <l.gatto@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-01-05 22:55:27 +0000

    finalize .addOverhang and .calculateHeavyLabels
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Pbase@98021 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/heavylabels-functions.R
+++ R/heavylabels-functions.R
@@ -2,9 +2,13 @@
 #' see https://github.com/sgibb/cleaver/issues/5 for details.
 #' TODO: There should be a function to find the best labels for a given protein
 #' automatically.
+#' @param peptides a named character vector of peptides (names must correspond
+#' to the protein accession numbers)
+#' @param proteins a cleaved Proteins object
+#' @param ... further arguments passed to .addOverhangs
 #' @noRd
 #'
-dummyAddOverhang <- function(peptides, proteins, ...) {
+.calculateHeavyLabels <- function(peptides, proteins, ...) {
     stopifnot(is(peptides, "character"))
     stopifnot(is(proteins, "Proteins"))
 
@@ -12,25 +16,41 @@ dummyAddOverhang <- function(peptides, proteins, ...) {
         stop("No names for ", sQuote("peptides"), " available!")
     }
 
-    proteins <- proteins[unique(names(peptides))]
-
-    pos <- .peptidePosition(peptides, aa(proteins))
-    pr <- pranges(proteins)
     aa <- aa(proteins)
+    pr <- pranges(proteins)
+    pos <- .peptidePosition(peptides, aa)
 
-    shiftBy <- c(0L, cumsum(head(nchar(aa), -1L)))
-    pos <- .flatIRangesList(pos, shift = TRUE, shiftBy = shiftBy)
-    pr <- .flatIRangesList(pr, shift = TRUE, shiftBy = shiftBy)
+    l <- vector(mode = "list", length = length(pos))
 
-    pindex <- match(start(pr), start(pos))
-    pindex <- pindex[!is.na(pindex)]
+    ## TODO: Because .addOverhangs is only vectorized on peptide level (multiple
+    ## peptides per protein) and not on protein level we need to loop over all
+    ## proteins.
+    for (i in seq_along(pos)) {
+        pindex <- match(start(pos[[i]]), start(pr[[i]]))
+        pindex <- pindex[!is.na(pindex)]
 
-    print(pindex)
+        if (length(pindex)) {
+            l[[i]] <- cbind(Protein = names(aa)[i],
+                            .addOverhangs(sequence = unlist(aa[[i]]),
+                                          pindex = pindex,
+                                          pranges = pr[[i]], ...))
+        }
+    }
 
-    .addOverhang(sequence = unlist(aa), pindex = pindex, pranges = pr, ...)
+    d <- do.call(rbind, l)
+
+    ## create labeled peptide string
+    d$spikeTide <- apply(d, 1, function(x) {
+        paste0(na.omit(c(x["N_overhang"], x["Peptide"], x["C_overhang"])),
+               collapse=".")
+    })
+
+    return(d)
 }
 
-#' This functions creates peptide sequences for heavy labeled peptides.
+#' This functions creates peptide sequences for heavy labeled peptides. Sadly
+#' this function is not completely vectorized yet.
+#' TODO: implement method != "both"
 #' See https://github.com/sgibb/cleaver/issues/5 for details.
 #' @param sequence character/AAString/AAStringSet, protein sequence(s)
 #' @param pindex index (position) of the peptide(s) in the cleaved protein(s)
@@ -43,9 +63,14 @@ dummyAddOverhang <- function(peptides, proteins, ...) {
 #' @param method character, should the N/C terminus prefered or should they
 #' treated equaly; not implemented yet
 #' @noRd
-.addOverhang <- function(sequence, pindex, pranges, maxN = 20L,
-                         nN = 4L, nC = 3L, endsWith = c("K", "R", "G"),
-                         method = c("both", "N", "C")) {
+.addOverhangs <- function(sequence, pindex, pranges, maxN = 20L,
+                          nN = 4L, nC = 3L, endsWith = c("K", "R", "G"),
+                          method = c("both", "N", "C")) {
+    .isTRUE <- function(x) {
+        sapply(x, isTRUE)
+    }
+
+    sequence <- as.character(sequence)
     stopifnot(is(pranges, "IRanges"))
 
     method <- match.arg(method)
@@ -65,8 +90,8 @@ dummyAddOverhang <- function(peptides, proteins, ...) {
     pe <- end(pranges)[pindex]
     pw <- w[pindex]
 
-    iN <- which(w >= nN)
-    iC <- which(w >= nC)
+    iN <- which(w >= nN[1L])
+    iC <- which(w >= nC[1L])
 
     ## handle too long sequences
     isTooLarge <- pw > (maxN - nN - nC)
@@ -75,10 +100,14 @@ dummyAddOverhang <- function(peptides, proteins, ...) {
     solution[isTooLarge] <- "no_overhangs"
 
     ## find first valid cleavage product
-    before <- iN[findInterval(pindex, iN, rightmost.closed = FALSE,
-                              all.inside = FALSE) - 1L]
-    after <- iC[findInterval(pindex, iC, rightmost.closed = TRUE,
-                             all.inside = FALSE) + 1L]
+    ## shift iN left to avoid overflows
+    before <- c(iN[1L], iN)[findInterval(pindex, iN, rightmost.closed = FALSE,
+                                         all.inside = FALSE)]
+
+    ## shift iC right to avoid overflows:
+    after <- c(iC, tail(iC, 1L))[findInterval(pindex, iC,
+                                              rightmost.closed = TRUE,
+                                              all.inside = FALSE) + 1L]
 
     ## extend N- and C-terminus
     s <- pmax(end(pranges)[before] - (nN - 1L), 1L)
@@ -86,7 +115,7 @@ dummyAddOverhang <- function(peptides, proteins, ...) {
 
     ## test correct ending
     if (!is.null(endsWith)) {
-        isCorrectEnding <- substr(sequence, e, e) %in% endsWith
+        isCorrectEnding <- substring(sequence, e, e) %in% endsWith
         maxN[!isCorrectEnding] <- maxN[!isCorrectEnding] - 1L
         e[!isCorrectEnding] <- e[!isCorrectEnding] - 1L
     }
@@ -101,23 +130,21 @@ dummyAddOverhang <- function(peptides, proteins, ...) {
     nLength <- ps - s
 
     ## 3. shorten N/C or both
-    shortN <- !isValidLength & cLength <= nC
-    shortC <- !isValidLength & nLength <= nN
-    shortBoth <- !isValidLength & !shortN & !shortC
+    shortN <- .isTRUE(!isValidLength & cLength <= nC)
+    shortC <- .isTRUE(!isValidLength & nLength <= nN)
+    shortBoth <- .isTRUE(!isValidLength & !shortN & !shortC)
 
-    s[shortN] <- ps - (maxN - pw - nC)
-    e[shortC] <- pe + (maxN - pw - nN)
-    s[shortBoth] <- ps - nN
-    e[shortBoth] <- pe + nC
+    s[shortN] <- (ps - (maxN - pw - nC))[shortN]
+    e[shortC] <- (pe + (maxN - pw - nN))[shortC]
+    s[shortBoth] <- (ps - nN)[shortBoth]
+    e[shortBoth] <- (pe + nC)[shortBoth]
     solution[shortN] <- "N_overhang_shortened"
     solution[shortC] <- "C_overhang_shortened"
     solution[shortBoth] <- "both_overhangs_shortened"
 
-    ## TODO: return something more useful
-    #return(list(pranges=IRanges(s, e), solution=solution))
-    return(data.frame(Peptide = substr(sequence, ps, pe),
-                      N_overhang = substr(sequence, s, ps - 1L),
-                      C_overhang = substr(sequence, pe + 1L, e),
+    return(data.frame(Peptide = substring(sequence, ps, pe),
+                      N_overhang = substring(sequence, s, ps - 1L),
+                      C_overhang = substring(sequence, pe + 1L, e),
                       spikeTideResult = solution, stringsAsFactors = FALSE))
 }
 
