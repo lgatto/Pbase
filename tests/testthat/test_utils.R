358556cd25198fb834bbd91b6143b8feaa074ace
l.gatto <l.gatto@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-02-27 21:00:18 +0000

    Merge branch 'master' into remotes/hedgehog
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Pbase@99950 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -44,31 +44,6 @@ test_that(".flatIRangesList", {
 
 })
 
-test_that(".splitIRanges", {
-    expect_error(Pbase:::.splitIRanges(list()))
-
-    ir <- IRanges(start = c(1, 5, 17, 20), end = c(3, 10, 19, 25))
-
-    result <- IRangesList(IRanges(start = 1, end = 3),
-                          IRanges(start = 5, end = 10),
-                          IRanges(start = 17, end = 19),
-                          IRanges(start = 20, end = 25))
-    names(result) <- as.character(1:4)
-
-    resultSplit <- IRangesList(IRanges(start = c(1, 5), end = c(3, 10)),
-                               IRanges(start = c(17, 20), end = c(19, 25)))
-    names(resultSplit) <- as.character(1:2)
-
-    resultUnshift <- IRangesList(IRanges(start = c(1, 5), end = c(3, 10)),
-                                 IRanges(start = c(7, 10), end = c(9, 15)))
-    names(resultUnshift) <- as.character(1:2)
-
-    expect_equal(Pbase:::.splitIRanges(ir), result)
-    expect_equal(Pbase:::.splitIRanges(ir, f = c(1, 1, 2, 2)), resultSplit)
-    expect_equal(Pbase:::.splitIRanges(ir, f = c(1, 1, 2, 2), unshift = TRUE),
-                                       resultUnshift)
-})
-
 test_that(".setNames2", {
     x <- 1:3
     nm1 <- LETTERS[1:3]
